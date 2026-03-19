aa425c948f201dea839b9803737ad1f62fa28da7
eyahammar <eya.hammar@insat.ucar.tn>
2026-03-19 16:14:37 +0100

    login and registration endpoints

---
+++ backend/database.py
@@ -0,0 +1,26 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+engine = create_engine(DATABASE_URL)#for database connection
+
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
+
+Base = declarative_base()#any model is database table
+
+
+def get_db():#inject db session
+    db = SessionLocal()
+    try:
+        yield db#sends the session to the endpoint
+    finally:
+        db.close()
---
+++ backend/main.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+from .database import Base, engine
+from .routes import auth_routes
+
+Base.metadata.create_all(bind=engine)
+
+app=FastAPI()
+
+app.include_router(auth_routes.router)
+
+
+@app.get("/")
+def root():
+    return {"message": "app running"}
